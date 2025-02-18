1c7c8e372cb2d241ccd80e377be07a6830c8ef3b
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2025-02-18 14:26:41 +0530

    Update PlanFormPage.jsx

--- src/components/PlanFormPage.jsx
+++ src/components/PlanFormPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import DashHeader from './DashHeader'
 import './PlanFormPage.css'
 import Footer from './Footer'
-
+import TravelPlanningForm from './TravelPlanningForm'
 const PlanFormPage = () => {
   return (
     <>
@@ -13,48 +13,8 @@ const PlanFormPage = () => {
             </div>
             
             <div className='PlanForm'>
-  <form>
-    <center><h2>Plan Trip</h2></center>
-    <div className="form-group form-group-locations">
-      <div>
-        <label htmlFor="currentLocation">Current Location:</label>
-        <input type="text" placeholder="Enter your current location" required />
-      </div>
-
-      <div>
-      <label htmlFor="destination">Destination:</label>
-      <input type="text" placeholder="Enter your destination" required/>
-      </div>
-    </div>
-
-    <div className="form-group">
-      <label htmlFor="noOfDays">Number of Days to Explore:</label>
-      <input
-        type="number"
-        id="noOfDays"
-        name="noOfDays"
-        placeholder="Enter number of days"
-        min="1"
-        required
-      />
-    </div>
-
-    <div className="form-group">
-      <label htmlFor="date">Start Date:</label>
-      <input
-        type="date"
-        id="date"
-        name="date"
-        required
-      />
-    </div>
-
-    <center>
-      <button type="submit" className="submit-button">Plan My Trip</button>
-    </center>
-
-  </form>
-</div>
+              <TravelPlanningForm/>
+            </div>
         </div>
         <Footer/>
     </>
